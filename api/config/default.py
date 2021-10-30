2a04b149b0d32881875ea1bcf06028e7ef75dc8e
Trastejant <nano@trastejant.com>
2021-10-30 12:35:40 +0200

    feat(api): Configuraciones

---
+++ api/config/default.py
@@ -0,0 +1,6 @@
+from os.path import abspath, dirname, join
+# Define the application directory
+BASE_DIR = dirname(dirname(abspath(__file__)))
+# Media dir
+MEDIA_DIR = join(BASE_DIR, 'media')
+POSTS_IMAGES_DIR = join(MEDIA_DIR, 'posts')
